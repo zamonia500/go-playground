0abb0f3c973c9b13312a5d6a2a8c6de4e9c66778
YoungCheol Jang <youngcheol.jang@makinarocks.ai>
2022-08-07 15:59:45 +0900

    Use type interface

--- go-sandbox/generics/main.go
+++ go-sandbox/generics/main.go
@@ -2,6 +2,10 @@ package main
 
 import "fmt"
 
+type Number interface {
+	int64 | float64
+}
+
 func SumInts(m map[string]int64) int64 {
 	var s int64
 	for _, v := range m {
@@ -18,7 +22,7 @@ func SumFloats(m map[string]float64) float64 {
 	return s
 }
 
-func SumIntsOrFloats[K comparable, V int64 | float64](m map[K]V) V {
+func SumIntsOrFloats[K comparable, V Number](m map[K]V) V {
 	var s V
 	for _, v := range m {
 		s += v
